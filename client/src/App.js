7c720f7d70ed93341412c0f331798e9378d0321e
milkcan <milkcanbass@gmail.com>
2020-03-21 21:34:28 -0400

    connection check

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log(process.env.GOOGLE_API_KEY);
   return (
     <div className='App'>
       <header className='App-header'>
